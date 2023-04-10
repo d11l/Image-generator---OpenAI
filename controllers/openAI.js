8db4cf85700e4a4ab5faa8674ea82443d17cc9d3
Abdulrahman radhi <80861137+d11l@users.noreply.github.com>
2023-04-11 02:00:55 +0300

    Update openAI.js

--- controllers/openAI.js
+++ controllers/openAI.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
